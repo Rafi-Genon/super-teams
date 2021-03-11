cd3f32acc6db13d7a17edb0eefc35e04eba267b6
Rafi-Genon <mdabulhasannurrafi@gmail.com>
2021-03-11 09:40:32 +0600

    footer change for deploy test

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div>
-            <p className="footer">Copy Right 2021 !! This site made by Md. AHN Rafi</p>
+            <p className="footer">Copy Right 2021 !! This site made by Md. AHN Rafi genon</p>
         </div>
     );
 };
